01bb9a5dec75e551786d277e4371c2171f437c58
Jens Neuber <jens.neuber@royalkomm.de>
2012-11-02 10:36:30 +0100

    working on it

--- jam/require.config.js
+++ jam/require.config.js
@@ -2,12 +2,17 @@ var jam = {
     "packages": [
         {
             "name": "jquery",
-            "location": "jam/jquery",
+            "location": "../jam/jquery",
             "main": "jquery.js"
         },
         {
+            "name": "less",
+            "location": "../jam/less",
+            "main": "./lib/index.js"
+        },
+        {
             "name": "modernizr",
-            "location": "jam/modernizr"
+            "location": "../jam/modernizr"
         }
     ],
     "version": "0.2.11",
