769de43a7fc72d163dbe8e982a49945d2123ca1e
Jens Neuber <jens.neuber@royalkomm.de>
2012-11-02 09:45:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+=======
+html5Jam
+========
+
+The HTML5-Boilerplate reduced to its essentials and prepared for some Jam.JS magic
+
---
+++ jam/require.config.js
@@ -0,0 +1,26 @@
+var jam = {
+    "packages": [
+        {
+            "name": "jquery",
+            "location": "jam/jquery",
+            "main": "jquery.js"
+        },
+        {
+            "name": "modernizr",
+            "location": "jam/modernizr"
+        }
+    ],
+    "version": "0.2.11",
+    "shim": {}
+};
+
+if (typeof require !== "undefined" && require.config) {
+    require.config({packages: jam.packages, shim: jam.shim});
+}
+else {
+    var require = {packages: jam.packages, shim: jam.shim};
+}
+
+if (typeof exports !== "undefined" && typeof module !== "undefined") {
+    module.exports = jam;
+}
